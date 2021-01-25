e1d15ae253d1e013db52417def0c02e6eea4f048
DESKTOP-2DTOUAQ\ehdrj <ehdrjs4773@naver.com>
2021-01-25 22:57:36 +0900

    1차 푸쉬

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -5,6 +5,11 @@ using System.Threading.Tasks;
 namespace ServerCore
 {
 
+    class FastLock
+    {
+       public int id;
+    }
+
     //메모리 배리어
     // A) 코드 재배치 억제
     // B) 가시성
@@ -12,7 +17,50 @@ namespace ServerCore
     // 1) Full Memory Barrier ( ASM MFENCE, C# Thread.MemoryBarrier) : Store/Load 둘다 막는다
     // 2) Store Memory Barrier ( ASM SFENCE) : Store 만 막는다
     // 3) Load Memory Barrier (ASM LFENCE) : Load 만 막는다.
+    
+   class SessionManager
+    {
 
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+
+    }
+    class UserManager
+    {
+
+        static object _lock = new object();
+
+        public static void Test()
+        {
+
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+    }
 
 
     class Program
@@ -27,15 +75,9 @@ namespace ServerCore
             // 집행검 User2 인벤에 넣어라
             // 집행검 User1 인벤에서 없애라
 
-            for(int i = 0; i < 1000000; i++)
+            for(int i = 0; i < 100; i++)
             {
-                //상호배제 Mutual Exclusive
-                //CriticalSection
-
-                lock (_obj)
-                {
-                    number++;
-                }
+                SessionManager.Test();
             }
         }
 
@@ -43,13 +85,9 @@ namespace ServerCore
         // 데드락 DeadLock 더이상 사용할수 없는 자물쇠
         static void Thread_2()
         {
-            for(int i =0;i < 1000000; i++)
+            for(int i =0;i < 100; i++)
             {
-                Monitor.Enter(_obj);
-
-                number--;
-
-                Monitor.Exit(_obj);
+                UserManager.Test();
             }
         }
 
@@ -60,6 +98,9 @@ namespace ServerCore
             Task t2 = new Task(Thread_2);
 
             t1.Start();
+
+            Thread.Sleep(100);
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
