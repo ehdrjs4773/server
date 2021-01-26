de98d9dd82eb6adebf1fb7ed9dd0a090cfce5eea
DESKTOP-2DTOUAQ\ehdrj <ehdrjs4773@naver.com>
2021-01-26 17:28:01 +0900

    WhireLock, ReadLock 구현

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,73 +4,43 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class SpinLock
-    {
-        volatile int _locked = 0;
-
-        public void Acquire()
-        {
-            while(true)
-            {
-                // CAS Compare-And-Swap
-                // if (_locked == 0)
-                //     _locked = 1;
-                int expected = 0;
-                int desired = 1;
-               if(Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
-                break;
-
-
-
-                //Thread.Sleep(1); // 무조건 휴식 => 무조건 1ms 정도 쉬고 싶어요
-                //Thread.Sleep(0); // 조건부 양보 => 나보다 우선순위가 낮은 애들한테는 양보 불가 => 우선 순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인한테
-                //Thread.Yield();  // 관대한 양보 => 관대하게 양보할테니, 지금 실행이 가능한 쓰레드가 있으면 실행하세요 => 실행 가능한 애가 없으면 남은 시간 소진
-
-            }
-         
-        }
-        public void Release()
-        {
-            _locked = 0;
-        }
-    }
 
 
     class Program
     {
-        static int _num = 0;
-        static SpinLock _lock = new SpinLock();
 
-        static void Thread_1()
-        {
-            for(int i = 0; i < 100000; i++)
-            {
-                _lock.Acquire();
-                _num++;
-                _lock.Release();
-            }
-        }
-        static void Thread_2()
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.Acquire();
-                _num--;
-                _lock.Release();
-            }
-        }
-
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
         static void Main(string[] args) // 메인 직원
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+
+            Task t1 = new Task(delegate ()
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                }
+            });
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
+
+
 
             t1.Start();
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(_num);
+            Console.WriteLine(count);
         }
     }
 }
