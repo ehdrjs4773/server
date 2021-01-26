1c0c799f02bbb0596cd789fd597213d20aa524d9
DESKTOP-2DTOUAQ\ehdrj <ehdrjs4773@naver.com>
2021-01-27 00:11:41 +0900

    소켓 네트워크 구현

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,34 +1,68 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServerCore
 {
 
-    // [ JobQueue ]
-
     class Program
     {
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name Is { Thread.CurrentThread.ManagedThreadId}"; });
-        
-        static void WhoAmI()
-        {
-            bool repeat = ThreadName.IsValueCreated;
-
-            if(repeat)
-                  Console.WriteLine(ThreadName.Value + "(repeat)");
-            else
-                Console.WriteLine(ThreadName.Value);
-        }
-
+ 
         static void Main(string[] args) // 메인 직원
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(3,3);
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            // DNS  (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost =  Dns.GetHostEntry(host);
+            IPAddress ipAddr =  ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 문지기 휴대폰
+            Socket listenSocket = new Socket(endPoint.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
 
 
-            ThreadName.Dispose();
+            try
+            {
+                // 문지기 교육
+                listenSocket.Bind(endPoint);
+
+                // 영업시작 
+                // backlog : 최대 대기수
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    // 손님을 입장시킨다
+
+                    Socket clientSocket = listenSocket.Accept();
+
+                    // 받는다
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    // 보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+                    clientSocket.Send(sendBuff);
+
+                    // 쫓아낸다
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+
+
+                }
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+           
         }
     }
 }
